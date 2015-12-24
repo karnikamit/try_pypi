bda1f4cd3cef8a01a0b5d891644d39c3097148db
Amit Karnik <amit.karnik@edgenetworks.in>
2015-12-24 10:55:09 +0530

    again

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = "karnikamit"
+from setuptools import setup
+
+setup(name='hello_world_amit',
+      version='0.1',
+      description='Hello world',
+      url='https://github.com/karnikamit/try_pypi.git',
+      author='karnikamit',
+      author_email='karnikamit46@gmail.com',
+      license='MIT',
+      packages=['try_pypi'],
+      zip_safe=False)
---
+++ try_pypi/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = "karnikamit"
+
+
+def hello_world(name):
+    return "%s says 'Hello World.'" % name
